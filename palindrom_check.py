1b34c9a83383f584ba2b80de415be2a9826f630e
diver-ofthe-datalake <44242709+diver-ofthe-datalake@users.noreply.github.com>
2018-10-17 16:09:56 -0400

    Update palindrom_check.py

--- palindrom_check.py
+++ palindrom_check.py
@@ -1,10 +1,11 @@
+#Checking an input string is Palindrome or not
 def checking_palindrome (given_input_string):
-   filtered_input = []
+   filtered_input = []                                      #define list to do filtering string input
    for elem in given_input_string:
         if 'a' <= elem <= 'z' or 'A' <= elem <= 'Z' or '0' <= elem <= '9': 
             filtered_input.append(elem)
-   filtered_string = ''.join(filtered_input)
-   filtered_string.lower()
+   filtered_string = ''.join(filtered_input)                #converting list back to string
+   filtered_string.lower()                                  
    if filtered_string == filtered_string[::-1]:
     print ('The input','"',given_input,'"','is palindrom')
    else:
